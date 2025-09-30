20e266822b5d5a24ce29fedc4ee43e21ca3efedf
fevunge <developervunge@gmail.com>
2025-09-30 13:05:28 +0100

    chore: refactor AddLink function to accept echo.Echo parameter

--- controller/link.go
+++ controller/link.go
@@ -1,6 +1,12 @@
 // Package controller handles the HTTP requests and responses for link-related operations.
 package controller
 
-func AddLink() {
-	
+import "github.com/labstack/echo/v4"
+
+func AddLink(e *echo.Echo) error {
+	link := e.Group("/links")
+	link.POST("/", func(c echo.Context) error {
+		c.JSON(201, ["OK"])
+	})
+	return nil
 }
