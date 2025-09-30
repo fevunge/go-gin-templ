653e62cf63c451d33d99c7c15ef925a86a6e7f21
fevunge <developervunge@gmail.com>
2025-09-30 13:05:31 +0100

    chore: implement GetProfile function for user retrieval

--- controller/user.go
+++ controller/user.go
@@ -21,3 +21,17 @@ func CreateUser(e *echo.Echo) {
 		return c.JSON(201, response)
 	})
 }
+
+func GetProfile(e *echo.Echo) {
+	user := e.Group("/users")
+	user.GET("/profile", func(c echo.Context) error {
+		username := c.QueryParam("username")
+
+		response, err := model.GetProfile(username)
+		if err != nil {
+			errorMessage := map[string]string{"error": err.Error()}
+			return c.JSON(500, errorMessage)
+		}
+		return c.JSON(200, response)
+	})
+}
