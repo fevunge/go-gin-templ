795fd205dfb85d4ca45ee15a8a43c2842e7afbb9
fevunge <developervunge@gmail.com>
2025-10-01 09:03:53 +0100

    feat(controller): remove unused AddLink function

--- controller/link.go
+++ controller/link.go
@@ -1,12 +1,2 @@
 // Package controller handles the HTTP requests and responses for link-related operations.
 package controller
-
-import "github.com/labstack/echo/v4"
-
-func AddLink(e *echo.Echo) error {
-	link := e.Group("/links")
-	link.POST("/", func(c echo.Context) error {
-		return c.String(201, "OK")
-	})
-	return nil
-}
