71a7626dc9e59d07f0b281d43b4a8a74622f640a
fevunge <developervunge@gmail.com>
2025-10-01 12:15:10 +0100

    feat: add function to retrieve user by username using Prisma

--- repository/user.go
+++ repository/user.go
@@ -3,17 +3,42 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/fevunge/let-go/entity"
 	"github.com/fevunge/let-go/prisma/db"
 	"github.com/fevunge/let-go/service/database"
 )
 
-var prisma = database.PrismaClient()
+var (
+	prisma = database.PrismaClient()
+	ctxt   = context.Background()
+)
+
+func GetUserByUsername(username string) (*db.UserModel, error) {
+	user, err := prisma.User.FindUnique(
+		db.User.Username.Equals(username),
+	).Exec(ctxt)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func validUsername(username string) bool {
+	if user, err := GetUserByUsername(username); err == nil {
+		if user != nil {
+			return false
+		}
+		return true
+	}
+	return true
+}
 
 func SaveUser(user entity.User) (*db.UserModel, error) {
-	ctxt := context.Background()
-
+	if !validUsername(user.Username) {
+		return nil, errors.New("invalid username")
+	}
 	created, err := prisma.User.CreateOne(
 		db.User.Username.Set(user.Username),
 		db.User.Password.Set(user.Password),
@@ -21,7 +46,7 @@ func SaveUser(user entity.User) (*db.UserModel, error) {
 	).Exec(ctxt)
 
 	if created != nil {
-		return created, err
+		return created, nil
 	}
 	return nil, err
 }
