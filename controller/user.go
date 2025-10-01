c824e55f349949e31f05d8cef1efe5d008771b69
fevunge <developervunge@gmail.com>
2025-10-01 09:59:16 +0100

    refactor(user): rename route "/" to "/create" for user creation

--- controller/user.go
+++ controller/user.go
@@ -6,12 +6,24 @@ import (
 )
 
 func CreateUser(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/create", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"name":     "John Doe",
 			"email":    "Johndoe@email.com",
 			"age":      30,
 			"is_admin": false,
+			"me":       c.ClientIP(),
+		})
+	})
+}
+
+func GetName(r *gin.Engine) {
+	data := Param{}
+
+	r.GET("/get", func(c *gin.Context) {
+		c.BindJSON(&data)
+		c.JSON(200, gin.H{
+			"query": data.Fields,
 		})
 	})
 }
