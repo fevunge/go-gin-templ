83c5013e53de874e067720652bb296d67c133fba
fevunge <developervunge@gmail.com>
2025-10-01 04:18:18 +0100

    fix(link): correct response type for POST request

--- controller/link.go
+++ controller/link.go
@@ -6,7 +6,7 @@ import "github.com/labstack/echo/v4"
 func AddLink(e *echo.Echo) error {
 	link := e.Group("/links")
 	link.POST("/", func(c echo.Context) error {
-		c.JSON(201, ["OK"])
+		return c.String(201, "OK")
 	})
 	return nil
 }
