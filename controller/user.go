f3007284b93e07793713813d94c68df9be1f5d0e
fevunge <developervunge@gmail.com>
2025-10-01 10:12:23 +0100

    feat: update GetName endpoint to take an ID parameter

--- controller/user.go
+++ controller/user.go
@@ -2,6 +2,9 @@
 package controller
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,12 +21,27 @@ func CreateUser(r *gin.Engine) {
 }
 
 func GetName(r *gin.Engine) {
-	data := Param{}
+	data := []string{"Banana", "Apple", "Orange"}
 
-	r.GET("/get", func(c *gin.Context) {
-		c.BindJSON(&data)
-		c.JSON(200, gin.H{
-			"query": data.Fields,
+	r.GET("/get/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "ID must be a number",
+				"result":  nil,
+			})
+			return
+		}
+		if id > len(data) || id <= 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "Index out of range 1-" + strconv.Itoa(len(data)),
+				"result":  nil,
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Everything is OK",
+			"result":  data[id-1],
 		})
 	})
 }
