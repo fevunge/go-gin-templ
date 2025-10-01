196412caab446c5241e944b26055e2a2d921ffb6
fevunge <developervunge@gmail.com>
2025-10-01 09:04:03 +0100

    refactor: replace echo framework with gin for routing in user controller

--- controller/user.go
+++ controller/user.go
@@ -2,33 +2,16 @@
 package controller
 
 import (
-	"github.com/fevunge/let-go/model"
-	"github.com/labstack/echo/v4"
+	"github.com/gin-gonic/gin"
 )
 
-func Login(e *echo.Echo) {
-	user := e.Group("/users")
-	user.POST("/login", func(c echo.Context) error {
-		username := c.FormValue("username")
-		password := c.FormValue("password")
-
-		c.Logger().Info("Login attempt for user:", username, password)
-		return c.JSON(200, map[string]string{"message": "Login successful"})
-	})
-}
-
-func CreateUser(e *echo.Echo) {
-	user := e.Group("/users")
-	user.POST("/", func(c echo.Context) error {
-		name := c.FormValue("name")
-		username := c.FormValue("username")
-		password := c.FormValue("password")
-
-		response, err := model.CreateUser(name, username, password)
-		if err != nil {
-			errorMessage := map[string]string{"error": err.Error()}
-			return c.JSON(500, errorMessage)
-		}
-		return c.JSON(201, response)
+func CreateUser(r *gin.Engine) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name":     "John Doe",
+			"email":    "Johndoe@email.com",
+			"age":      30,
+			"is_admin": false,
+		})
 	})
 }
